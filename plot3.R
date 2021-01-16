0fc316cc0587e6033faad7111fadcec13c9e039a
katyainiR <katyaini.ranjan@outlook.com>
2021-01-17 04:40:33 +0530

    Plot 3: sub_meter levels (all 3) vs datetime variable

---
+++ plot3.R
@@ -0,0 +1,20 @@
+url = "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+download.file(url, destfile = "Week1.zip")
+dataset<- read.csv(unz("Week1.zip", "household_power_consumption.txt"), sep = ';')
+dataset$Date<- as.POSIXct(strptime(dataset$Date, format = "%d/%m/%Y"))
+r<- dataset$Date =="2007-02-01" | dataset$Date == "2007-02-02"
+set<- dataset[r,]
+rm(dataset)
+datetime<- paste(set$Date, set$Time, sep = " ")
+datetime <- as.POSIXct(datetime, format= "%Y-%m-%d %H:%M:%S" )
+set$Sub_metering_1<- as.numeric(set$Sub_metering_1)
+set$Sub_metering_2<- as.numeric(set$Sub_metering_2)
+set$Sub_metering_3<- as.numeric(set$Sub_metering_3)
+plot(datetime, set$Sub_metering_1, type="n", ylab="Energy Sub metering", xlab="")
+lines(datetime, set$Sub_metering_1)
+lines(datetime, set$Sub_metering_2, col="red")
+lines(datetime, set$Sub_metering_3, col="blue")
+legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "sub_metering_3"), lty = 1, col = c("black", "red", "blue"))
+dev.copy(png, file = "plot3.png", height =480, width = 480)
+dev.off()
+dev.off()
